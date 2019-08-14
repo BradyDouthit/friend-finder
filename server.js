077bcdd6fbbba9cd8498e6c71213daec25de2735
Brady Douthit <brady.douthit99@gmail.com>
2019-08-14 13:53:25 -0400

    Set up the server.js and the html pages

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+let PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/home.html"));
+});
+
+app.get("/survey", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/survey.html"));
+});
+
+
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
+});
