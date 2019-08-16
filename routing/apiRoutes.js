8de2983cdb1d01e4d722cdeba02dc589ab6acb34
Brady Douthit <brady.douthit99@gmail.com>
2019-08-16 19:07:42 -0400

    post now works

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,11 +1,24 @@
 let path = require("path");
-
+let data = require("../app/data/friends.js")
 module.exports = function(app) {
     app.get("/api/friends", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/home.html"));
+        res.json(data)
     });
     
     app.post("/api/friends", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/survey.html"));
+        // console.log(data)
+        // var scoresParsed = [];
+        // for (var i = 0; i < data.length; i++) {
+        //     for (var j = 0; j < data[i].scores.length; j++) {
+        //         var parsed = parseInt(data[i].scores[j])
+        //         scoresParsed.push(parsed)
+        //     }
+        // }
+        // if (scoresParsed.length >= 10) {
+
+        // }
+        // console.log(`Parsed: ${scoresParsed}`)
+        data.push(req.body);
+        res.json(true);
     });
 };
